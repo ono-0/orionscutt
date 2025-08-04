0f8cdbdb99165b3373e57d395693c78446796f87
onomatopoeia <113570502+ono-0@users.noreply.github.com>
2025-08-04 15:52:03 +0000

    new file:   alphabetshiftcipherdecrypt.py renamed:    alphabetshiftcipher.py -> alphabetshiftcipherencrypt.py

---
+++ alphabetshiftcipherdecrypt.py
@@ -0,0 +1,11 @@
+ciphertext = input("enter ciphertext")
+key = int(input("enter key"))
+ciphertextlength = len(ciphertext)
+plaintext = ""
+listplace = 0
+ciphertextlist = list(ciphertext)
+while ciphertextlength != listplace:
+    plaintextpart = chr(ord(ciphertextlist[listplace]) + -key)
+    plaintext = plaintext + plaintextpart
+    listplace = listplace + 1
+print(plaintext)
