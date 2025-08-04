634e9f05abea7b701fe74ea9bd81cc97882a2526
onomatopoeia <113570502+ono-0@users.noreply.github.com>
2025-08-04 19:30:27 +0000

    modified:   alphabetshiftcipher/alphabetshiftcipherdecrypt.py modified:   alphabetshiftcipher/alphabetshiftcipherencrypt.py

--- alphabetshiftcipher/alphabetshiftcipherdecrypt.py
+++ alphabetshiftcipher/alphabetshiftcipherdecrypt.py
@@ -1,11 +1,12 @@
-ciphertext = input("enter ciphertext")
-key = int(input("enter key"))
-ciphertextlength = len(ciphertext)
-plaintext = ""
-listplace = 0
-ciphertextlist = list(ciphertext)
-while ciphertextlength != listplace:
-    plaintextpart = chr(ord(ciphertextlist[listplace]) + -key)
-    plaintext = plaintext + plaintextpart
-    listplace = listplace + 1
-print(plaintext)
+def abcdecrypt():
+    ciphertext = input("enter ciphertext")
+    key = int(input("enter key"))
+    ciphertextlength = len(ciphertext)
+    plaintext = ""
+    listplace = 0
+    ciphertextlist = list(ciphertext)
+    while ciphertextlength != listplace:
+        plaintextpart = chr(ord(ciphertextlist[listplace]) + -key)
+        plaintext = plaintext + plaintextpart
+        listplace = listplace + 1
+    print(plaintext)
--- alphabetshiftcipher/alphabetshiftcipherencrypt.py
+++ alphabetshiftcipher/alphabetshiftcipherencrypt.py
@@ -1,15 +1,16 @@
-f = open("ciphertextdatabase.log", "a")
-plaintext = input("enter plaintext")
-key = int(input("enter key"))
-plaintextlength = len(plaintext)
-ciphertext = ""
-listplace = 0
-plaintextlist = list(plaintext)
-while plaintextlength != listplace:
-    ciphertextpart = chr(ord(plaintextlist[listplace]) + key)
-    ciphertext = ciphertext + ciphertextpart
-    listplace = listplace + 1
-print(ciphertext)
-with open("ciphertextdatabase.log", "a") as f:
-    f.write(ciphertext)
-    f.write("\n")
+def abcencrypt():
+    f = open("ciphertextdatabase.log", "a")
+    plaintext = input("enter plaintext")
+    key = int(input("enter key"))
+    plaintextlength = len(plaintext)
+    ciphertext = ""
+    listplace = 0
+    plaintextlist = list(plaintext)
+    while plaintextlength != listplace:
+        ciphertextpart = chr(ord(plaintextlist[listplace]) + key)
+        ciphertext = ciphertext + ciphertextpart
+        listplace = listplace + 1
+    print(ciphertext)
+    with open("ciphertextdatabase.log", "a") as f:
+        f.write(ciphertext)
+        f.write("\n")
